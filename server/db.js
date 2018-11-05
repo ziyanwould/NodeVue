0ae62e063362a4fcfa33904834e98c3057d3bc7e
ziyanwould <448736378@qq.com>
2018-11-05 13:42:31 +0800

    远程服务器连接

--- server/db.js
+++ server/db.js
@@ -1,17 +1,33 @@
 /*
  * Description: 数据库配置
- * User: zhaoyiming
- * Date: 2017/9/16
+ * User       : zhaoyiming
+ * Date       : 2017/9/16
 */
+
+// const mysql = require('mysql');
+
+// module.exports = (sql, callback) => {
+// 	const db = mysql.createConnection({
+//         host    : 'localhost',
+//         user    : 'root',
+//         password: 'root',
+//         database: 'tour'
+//     });
+
+//     db.connect();
+//     db.query(sql, callback);
+// 	db.end();
+// };
 
 const mysql = require('mysql');
 
 module.exports = (sql, callback) => {
 	const db = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : 'root',
-        database : 'tour'
+        host    : '114.215.86.207',
+        user    : 'user',
+        password: 'user',
+        database: 'tour',
+        port    : 3306,
     });
 
     db.connect();
